a333b4dcd492b7a8eec81dabb72a7f3728b59022
Kakeru <skakeru0920@gmail.com>
2025-11-05 11:55:32 +0900

    test

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const knexConfig = require('./knexfile');
+console.log(DATABASE_URL, 'url');
 const environment = process.env.DATABASE_URL ? 'production' : 'development';
-console.log(environment, "this is settings")
+console.log(environment, 'this is settings');
 
 module.exports = knex(knexConfig[environment]);
