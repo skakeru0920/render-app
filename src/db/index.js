5551b481436c7b276402c5f6f656effa83922e16
Kakeru <skakeru0920@gmail.com>
2025-11-05 11:56:10 +0900

    test

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const knexConfig = require('./knexfile');
-console.log(DATABASE_URL, 'url');
+console.log(process.env.DATABASE_URL, 'url');
 const environment = process.env.DATABASE_URL ? 'production' : 'development';
 console.log(environment, 'this is settings');
 
