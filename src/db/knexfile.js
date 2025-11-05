2f1b6e74389242ac5e14d2e16dd0fad01ed4db93
Kakeru <skakeru0920@gmail.com>
2025-11-05 15:58:49 +0900

    test v2

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,39 +1,39 @@
-// const path = require('path');
-// module.exports = {
-//   development: {
-//     client: 'pg',
-//     connection: {
-//       database: 'ccpixels',
-//       user: 'postgres',
-//       password: null,
-//     },
-//     migrations: {
-//       directory: './data/migrations',
-//     },
-//     seeds: { directory: './data/seeds' },
-//   },
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//     migrations: {
-//       directory: path.resolve(__dirname, './data/migrations'),
-//     },
-//     seeds: { directory: path.resolve(__dirname, './data/seeds') },
-//   },
-// };
-
+const path = require('path');
 module.exports = {
-  client: 'pg',
-  connection: process.env.DATABASE_URL || {
-    database: 'ccpixels',
-    user: 'postgres',
-    password: null,
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'ccpixels',
+      user: 'postgres',
+      password: null,
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
   },
-  migrations: {
-    directory: './data/migrations',
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    migrations: {
+      directory: path.resolve(__dirname, './data/migrations'),
+    },
+    seeds: { directory: path.resolve(__dirname, './data/seeds') },
   },
-  seeds: { directory: './data/seeds' },
 };
+
+// module.exports = {
+//   client: 'pg',
+//   connection: process.env.DATABASE_URL || {
+//     database: 'ccpixels',
+//     user: 'postgres',
+//     password: null,
+//   },
+//   migrations: {
+//     directory: './data/migrations',
+//   },
+//   seeds: { directory: './data/seeds' },
+// };
